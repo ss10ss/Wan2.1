e8820d0ac8912407bb6b50c6fea2a0f03fdc3032
Didier Durand <didduran@amazon.ch>
2025-03-01 05:58:29 +0100

    Adding env variables for parametrized Gradio login

--- gradio/t2v_1.3B_singleGPU.py
+++ gradio/t2v_1.3B_singleGPU.py
@@ -204,4 +204,8 @@ if __name__ == '__main__':
     print("done", flush=True)
 
     demo = gradio_interface()
-    demo.launch(server_name="0.0.0.0", share=False, server_port=7860)
+    if "WAN_USER" in os.environ and "WAN_PWD" in os.environ:
+        demo.launch(server_name="0.0.0.0", share=False, server_port=7860,
+                    auth=(os.environ["WAN_USER"], os.environ["WAN_PWD"]))
+    else:
+        demo.launch(server_name="0.0.0.0", share=False, server_port=7860)
